ef77d6affed6b494d30d28bbcd7c9767d3232541
asd123745 <63216350+asd123745@users.noreply.github.com>
2020-06-01 09:42:50 +0900

    Add files via upload

---
+++ a.java
@@ -0,0 +1,26 @@
+package a;
+class E{
+	String n;
+	int id;
+	int sa;
+	boolean c;
+}
+public class a {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		E m = new E();
+		m.n="ȫ�浿";
+		m.id = 17;
+		m.sa=45;
+		m.c=true;
+		System.out.printf("%s\n",m.n);
+		System.out.printf("%s\n",m.id);
+		System.out.printf("%s\n",m.sa);
+		System.out.printf("%s\n",m.c);
+
+	}
+}
+
